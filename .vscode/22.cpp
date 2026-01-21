a390cc4d1993c43d170b07afbbc882f4267daf76
kotvitska-e-alt <kotvitska-e@class.ztk.org.ua>
2026-01-21 09:14:58 +0000

    ппп

---
+++ .vscode/22.cpp
@@ -0,0 +1,7 @@
+include <iostream>
+
+int main() {
+    std::cout << "візитка" << std::endl;
+    std::cout << "котвіцька євгенія" << std::endl;
+    return 0;
+}
---
+++ 21.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Kotvitska" << std::endl;
+    std::cout << "Evgenia" << std::endl;
+    return 0;
+}
